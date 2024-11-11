1deb7135ee395b4a137cb351cd291d4247da7a63
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-11 16:06:14 +0600

    added transaction page

---
+++ app/user/(private)/transactions/page.tsx
@@ -0,0 +1,34 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const Transactions = () => {
+  return (
+    <div className="my-4 mx-2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">All Transactions</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {[1, 2, 3].map((i) => (
+              <li key={i} className="flex items-center justify-between">
+                <div className="flex items-center space-x-4">
+                  <Avatar>
+                    <AvatarFallback>{i}</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="font-medium">Cash Out {i}</p>
+                    <p className="text-sm text-gray-500">2023-06-{10 + i}</p>
+                  </div>
+                </div>
+                <span className="font-medium text-red-500">-$10{i}.00</span>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Transactions;
