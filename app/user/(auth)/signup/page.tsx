2970c0052f2ce52f5b9f3aa4794e186002cd6a7f
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-11 15:42:45 +0600

    ADDED registration page (user)

--- app/user/(auth)/signup/page.tsx
+++ app/user/(auth)/signup/page.tsx
@@ -3,36 +3,63 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="font-signika  my-4 md:max-w-xl md:mx-auto">
       <h1 className="text-center my-2 text-blue-600 font-bold text-3xl">
-        LOGIN
+        REGISTRATION
       </h1>
       <form className="mx-2 space-y-4">
         <div className="space-y-2">
+          <Label htmlFor="name" className="text-md">
+            Name:
+          </Label>
+          <Input
+            className="h-14"
+            type="text"
+            id="name"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="space-y-2">
           <Label htmlFor="email" className="text-md">
-            Email/Phone Number:{" "}
+            Email:
           </Label>
           <Input
             className="h-14"
             type="text"
             id="email"
-            placeholder="Email/Phone"
+            placeholder="Enter Your Email"
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="email" className="text-md">
+            Phone Number:
+          </Label>
+          <Input
+            className="h-14"
+            type="phone"
+            id="email"
+            placeholder="Enter your phone number"
           />
         </div>
         <div className="space-y-2">
           <Label htmlFor="pin" className="text-md">
             PIN
           </Label>
-          <Input className="h-14" type="password" id="pin" placeholder="PIN" />
+          <Input
+            className="h-14"
+            type="password"
+            id="pin"
+            placeholder="Choose a 5 Digit PIN"
+          />
         </div>
-        <Button size={"lg"} className="w-full">
-          Login
+        <Button type="submit" size={"lg"} className="w-full">
+          Submit
         </Button>
         <p className="text-center text-lg">
-          New user?
-          <Link href={"/user/signup"} className="text-blue-500 underline">
+          Already Registered?
+          <Link href={"/user/login"} className="text-blue-500 underline">
             {" "}
             Register Here
           </Link>
@@ -51,4 +78,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
