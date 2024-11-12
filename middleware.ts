3ca7ddb9e14662dcbdd13fca1e6fe81fcc4c7baa
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-12 23:45:09 +0600

    added middleware file

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  // Avoiding login and register paths
+  const authPrefixes = ["/user/login", "/user/signup"];
+  const { pathname } = req.nextUrl;
+  if (authPrefixes.some((prefix) => pathname.startsWith(prefix))) {
+    return NextResponse.next();
+  }
+
+  const user = "";
+
+  if (!user) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/user/:path*"],
+};
