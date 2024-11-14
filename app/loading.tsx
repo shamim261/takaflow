f21f11f6f492833e937e360ae1a66db13c3739fa
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-14 16:39:30 +0600

    add spiner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,16 @@
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div
+        className="inline-block h-14 w-14 animate-spin rounded-full border-4 border-solid border-blue-600 border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
