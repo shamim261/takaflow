9718a342dbf895cc7284a1214cf19ffe65ec0e5f
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-16 01:04:17 +0600

    added cash in functionality

---
+++ app/api/user/cashin/route.ts
@@ -0,0 +1,60 @@
+import Transaction, { ITransaction } from "@/models/transactionModel";
+import User from "@/models/userModel";
+import { getUser } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import bcrypt from "bcrypt";
+import { HydratedDocument } from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  await connectDB();
+  const user = await getUser();
+  const { number, amount: userAmount, pin } = await req.json();
+
+  const sender = await User.findOne({ phone: user?.phone });
+  const receiver = await User.findOne({ phone: number });
+  // check the valid receipt
+
+  if (!receiver || receiver?.status === "pending" || sender?.phone === number) {
+    return NextResponse.json(
+      { error: "Receipt should be a valid agent!" },
+      { status: 400 }
+    );
+  }
+  // check the minimum amount
+  const minimumAmount = 50;
+  const amount = Number(userAmount);
+  if (amount < minimumAmount) {
+    return NextResponse.json(
+      { error: `Minimum amount is ${minimumAmount} taka!` },
+      { status: 400 }
+    );
+  }
+
+  // check pin
+  const isMatch = await bcrypt.compare(pin, sender.hashedPin);
+  if (!isMatch) {
+    return NextResponse.json({ error: "Invalid PIN" }, { status: 400 });
+  }
+  const fee = 0;
+  try {
+    const transaction: HydratedDocument<ITransaction> = new Transaction({
+      senderId: sender._id,
+      receiverId: receiver._id,
+      type: "cash_out",
+      fee: fee,
+      amount: amount + fee,
+    });
+    await transaction.save();
+    // TODO: Complete the transaction
+    return NextResponse.json(
+      { success: "Transaction created successfully!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error!" },
+      { status: 500 }
+    );
+  }
+};
--- app/user/(private)/cashin/page.tsx
+++ app/user/(private)/cashin/page.tsx
@@ -1,27 +1,54 @@
 "use client";
 import AmountInput from "@/components/AmountInput";
+import ErrorComponent from "@/components/ErrorComponent";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import sendMoneyInput from "@/schemas/sendMoneyInput";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import cashInInput from "@/schemas/cashInInput";
+import { ErrorMessage } from "@/types";
+import getError from "@/utils/getError";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Spinner } from "@radix-ui/themes";
+import { AxiosError } from "axios";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 const CashIn = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const router = useRouter();
-  const { register, handleSubmit } = useForm<sendMoneyType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<cashInType>({
+    resolver: zodResolver(cashInInput),
+  });
+  const axiosSecure = useAxiosSecure();
 
-  // interface sendMoneyType {
-  //   number: number;
-  //   amount: number;
-  //   pin: number;
-  // }
-  type sendMoneyType = z.infer<typeof sendMoneyInput>;
-
-  const submitHandler = (data: sendMoneyType) => {
-    console.log(data);
+  type cashInType = z.infer<typeof cashInInput>;
+  const submitHandler = async (formData: cashInType) => {
+    setIsSubmitting(true);
+    setError("");
+    try {
+      const { data } = await axiosSecure.post("/api/user/cashin", {
+        ...formData,
+      });
+      toast.success("Transaction Complete!");
+      router.push("/user/dashboard");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const err = error as AxiosError<ErrorMessage>;
+        setError(getError(err));
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -43,9 +70,12 @@ const CashIn = () => {
             type="number"
             id="agentNo"
           />
+          <ErrorComponent error={errors?.number?.message} />
         </div>
         <div className="flex flex-col justify-center items-center space-y-4 ">
           <AmountInput {...register("amount")} />
+
+          <ErrorComponent error={errors?.amount?.message} />
           <Input
             required
             {...register("pin")}
@@ -53,12 +83,16 @@ const CashIn = () => {
             placeholder="Enter PIN"
             className="text-center border-slate-300"
           />
+          <ErrorComponent error={errors?.pin?.message} />
+
+          <ErrorComponent error={error} />
           <Button
+            disabled={isSubmitting}
             type="submit"
             size={"lg"}
             className="bg-blue-600 hover:bg-blue-700 w-full text-md "
           >
-            Cash In Request
+            {isSubmitting && <Spinner size={"3"} />} Cash In Request
           </Button>
         </div>
       </form>
