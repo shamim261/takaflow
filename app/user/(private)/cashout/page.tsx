9f4a660bc9aef8bb9e2d4eb71f56a2d97496853f
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-16 00:55:05 +0600

    added cashout funtionality

--- app/user/(private)/cashout/page.tsx
+++ app/user/(private)/cashout/page.tsx
@@ -1,34 +1,61 @@
 "use client";
 import AmountInput from "@/components/AmountInput";
+import ErrorComponent from "@/components/ErrorComponent";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import sendMoneyInput from "@/schemas/sendMoneyInput";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import cashoutInput from "@/schemas/cashoutInput";
+import { ErrorMessage } from "@/types";
+import getError from "@/utils/getError";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Spinner } from "@radix-ui/themes";
+import { AxiosError } from "axios";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 const CashOut = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const router = useRouter();
-  const { register, handleSubmit } = useForm<sendMoneyType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<cashoutType>({
+    resolver: zodResolver(cashoutInput),
+  });
+  const axiosSecure = useAxiosSecure();
 
-  // interface sendMoneyType {
-  //   number: number;
-  //   amount: number;
-  //   pin: number;
-  // }
-  type sendMoneyType = z.infer<typeof sendMoneyInput>;
-
-  const submitHandler = (data: sendMoneyType) => {
-    console.log(data);
+  type cashoutType = z.infer<typeof cashoutInput>;
+  const submitHandler = async (formData: cashoutType) => {
+    setIsSubmitting(true);
+    setError("");
+    try {
+      const { data } = await axiosSecure.post("/api/user/cashout", {
+        ...formData,
+      });
+      toast.success("Transaction Complete!");
+      router.push("/user/dashboard");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const err = error as AxiosError<ErrorMessage>;
+        setError(getError(err));
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
     <div className="flex flex-col justify-center space-y-3 mx-2 my-4 md:max-w-md md:mx-auto">
       <form onSubmit={handleSubmit(submitHandler)}>
         <div className="flex flex-col justify-center items-center space-y-2 ">
-          <h2 className="text-xl font-semibold text-blue-600">Cashout</h2>
+          <h2 className="text-xl font-semibold text-blue-600">Cash Out</h2>
 
           <Label className="text-sm w-full" htmlFor="agentNo">
             Enter Agent Number:
@@ -41,9 +68,14 @@ const CashOut = () => {
             type="number"
             id="agentNo"
           />
+          <ErrorComponent error={errors?.number?.message} />
         </div>
         <div className="flex flex-col justify-center items-center space-y-4 ">
           <AmountInput {...register("amount")} />
+          <p className="text-sm text-gray-500">
+            *charge will be 1.5% of amount
+          </p>
+          <ErrorComponent error={errors?.amount?.message} />
           <Input
             required
             {...register("pin")}
@@ -51,12 +83,16 @@ const CashOut = () => {
             placeholder="Enter PIN"
             className="text-center border-slate-300"
           />
+          <ErrorComponent error={errors?.pin?.message} />
+
+          <ErrorComponent error={error} />
           <Button
+            disabled={isSubmitting}
             type="submit"
             size={"lg"}
             className="bg-blue-600 hover:bg-blue-700 w-full text-md "
           >
-            Cashout
+            {isSubmitting && <Spinner size={"3"} />} Cash Out
           </Button>
         </div>
       </form>
---
+++ schemas/cashoutInput.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const cashoutInput = z.object({
+  number: z.string().length(11, "Enter a valid number!"),
+  amount: z.string().min(1, "Minimum is 50 Taka"),
+  pin: z.string().min(1, "Please enter PIN"),
+});
+
+export default cashoutInput;
