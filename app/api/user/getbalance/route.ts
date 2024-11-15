c0b2e38af95828620b0f0c1102bc44cc62be2b54
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-15 16:54:26 +0600

    added response status to api

--- app/api/user/getbalance/route.ts
+++ app/api/user/getbalance/route.ts
@@ -9,11 +9,14 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const payload = await isValidToken(token!);
     if (payload) {
       const { balance } = await User.findOne({ _id: payload._id });
-      return NextResponse.json(balance);
+      return NextResponse.json(balance, { status: 200 });
     } else {
-      return NextResponse.json({ error: "Invalid Token" });
+      return NextResponse.json({ error: "Invalid Token" }, { status: 400 });
     }
   } catch (error) {
-    return NextResponse.json({ error: "Internal Server Error" });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
