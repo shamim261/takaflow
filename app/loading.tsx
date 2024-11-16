5d7e3e50b7f7cb9fd0eba217230882617af4e4f8
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-16 16:39:20 +0600

    added homepage

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import Spinner from "@/components/Spinner";
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Spinner classNames="h-14 w-14" />
+      <Spinner classNames="!h-14 !w-14" />
     </div>
   );
 };
