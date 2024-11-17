422ba6e57b7127d1066db3c0a4ca2120fb013a0d
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-18 03:50:32 +0600

    added pagination in transaction page

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -6,17 +6,25 @@ import useAxiosSecure from "@/hooks/useAxiosSecure";
 import { Badge } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import { Store, User } from "lucide-react";
+import { useState } from "react";
+import Pagination from "./Pagination";
 interface Props {
   visible?: number;
 }
 
 const TransactionsTable = ({ visible }: Props) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(8);
+
   const axiosSecure = useAxiosSecure();
   const { data, isLoading, error } = useQuery({
     queryKey: ["transactions"],
-    queryFn: () => axiosSecure.get("/api/user/transactions"),
+    queryFn: () => axiosSecure.get(`/api/user/transactions`),
     staleTime: 0,
   });
+  const lastItemIndex = currentPage * itemsPerPage;
+  const firstItemIndex = lastItemIndex - itemsPerPage;
+  const currentItem = data?.data?.result.slice(firstItemIndex, lastItemIndex);
 
   let transactions;
 
@@ -31,82 +39,95 @@ const TransactionsTable = ({ visible }: Props) => {
       ) : transactions?.length === 0 ? (
         <p>No transactions found!</p>
       ) : (
-        transactions?.map((transaction) => (
-          <div
-            key={transaction._id}
-            className="flex items-center py-4 px-4 hover:bg-muted/50"
-          >
-            <p>{error?.message}</p>
-            <div className="mr-4">
-              {transaction.userSpecificType === "Cash Out" ? (
-                <div className="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center">
-                  <Store className="w-6 h-6 text-blue-500" />
-                </div>
-              ) : (
-                <Avatar className="w-12 h-12 bg-blue-100 flex justify-center items-center">
-                  <User strokeWidth="2.5" className="h-6 w-6 text-blue-500 " />
-                </Avatar>
-              )}
-            </div>
-            <div className="flex-1 min-w-0">
-              <div className="flex items-center justify-between mb-1">
-                <h3 className="text-base font-medium text-foreground truncate">
-                  {transaction.userSpecificType}
-                </h3>
-                <Badge
-                  // variant={
-                  //   transaction.status === "completed" ? "success" : "warning"
-                  // }
-                  color={
-                    transaction.status === "rejected"
-                      ? "red"
-                      : transaction.status === "approved"
-                      ? "green"
-                      : undefined
-                  }
-                  className="ml-2"
-                >
-                  {transaction.status}
-                </Badge>
+        <>
+          {currentItem?.map((transaction) => (
+            <div
+              key={transaction._id}
+              className="flex items-center py-4 px-4 hover:bg-muted/50"
+            >
+              <p>{error?.message}</p>
+              <div className="mr-4">
+                {transaction.userSpecificType === "Cash Out" ? (
+                  <div className="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center">
+                    <Store className="w-6 h-6 text-blue-500" />
+                  </div>
+                ) : (
+                  <Avatar className="w-12 h-12 bg-blue-100 flex justify-center items-center">
+                    <User
+                      strokeWidth="2.5"
+                      className="h-6 w-6 text-blue-500 "
+                    />
+                  </Avatar>
+                )}
               </div>
-              <p className="text-sm text-muted-foreground truncate">
-                {transaction.userSpecificType === "Cash Out" || "Sent Money"
-                  ? transaction.receiverPhone
-                  : transaction.senderPhone}
-              </p>
-            </div>
-            <div className="text-right ml-4">
-              <p
-                className={`text-base font-medium ${
-                  transaction.status === "rejected" ||
+              <div className="flex-1 min-w-0">
+                <div className="flex items-center justify-between mb-1">
+                  <h3 className="text-base font-medium text-foreground truncate">
+                    {transaction.userSpecificType}
+                  </h3>
+                  <Badge
+                    // variant={
+                    //   transaction.status === "completed" ? "success" : "warning"
+                    // }
+                    color={
+                      transaction.status === "rejected"
+                        ? "red"
+                        : transaction.status === "approved"
+                        ? "green"
+                        : undefined
+                    }
+                    className="ml-2"
+                  >
+                    {transaction.status}
+                  </Badge>
+                </div>
+                <p className="text-sm text-muted-foreground truncate">
+                  {transaction.userSpecificType === "Cash Out" || "Sent Money"
+                    ? transaction.receiverPhone
+                    : transaction.senderPhone}
+                </p>
+              </div>
+              <div className="text-right ml-4">
+                <p
+                  className={`text-base font-medium ${
+                    transaction.status === "rejected" ||
+                    transaction.userSpecificType === "Received Money" ||
+                    transaction.userSpecificType === "Cash In"
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {transaction.status === "rejected" ||
                   transaction.userSpecificType === "Received Money" ||
                   transaction.userSpecificType === "Cash In"
-                    ? "text-green-600"
-                    : "text-red-600"
-                }`}
-              >
-                {transaction.status === "rejected" ||
-                transaction.userSpecificType === "Received Money" ||
-                transaction.userSpecificType === "Cash In"
-                  ? "+"
-                  : "-"}{" "}
-                ৳{transaction.amount.toFixed(2)}
-              </p>
-              <p className="text-sm text-muted-foreground">
-                {new Date(transaction.updatedAt).toLocaleString(undefined, {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: true, // Set to true for 12-hour format
-                  second: undefined, // Exclude seconds
-                })}
-              </p>
-              {/* <ChevronRight className="w-5 h-5 text-muted-foreground inline-block" /> */}
+                    ? "+"
+                    : "-"}{" "}
+                  ৳{transaction.amount.toFixed(2)}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {new Date(transaction.updatedAt).toLocaleString(undefined, {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true, // Set to true for 12-hour format
+                    second: undefined, // Exclude seconds
+                  })}
+                </p>
+                {/* <ChevronRight className="w-5 h-5 text-muted-foreground inline-block" /> */}
+              </div>
             </div>
-          </div>
-        ))
+          ))}
+          {!visible && (
+            <Pagination
+              postsPerPage={itemsPerPage}
+              currentPage={currentPage}
+              totalPosts={data?.data?.result?.length}
+              setCurrentPage={setCurrentPage}
+            />
+          )}
+        </>
       )}
     </>
   );
