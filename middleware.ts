e37aeda4473e16e56badfa01ac0c7e5db65d862e
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-18 17:07:01 +0600

    updated middleware for all roles

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 import isAuth from "@/utils/isAuth";
+import { decodeJwt } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 import { isTokenExpired } from "./utils/tokenUtils";
 
 export async function middleware(req: NextRequest) {
   // Avoiding login and register paths
-  const authPrefixes = ["/user/login", "/user/signup"];
+  const authPrefixes = [
+    "/user/login",
+    "/user/signup",
+    "/agent/login",
+    "/agent/signup",
+  ];
   const { pathname } = req.nextUrl;
   if (authPrefixes.some((prefix) => pathname.startsWith(prefix))) {
     return NextResponse.next();
@@ -18,14 +24,29 @@ export async function middleware(req: NextRequest) {
     );
   }
 
-  const userInfo = await isAuth();
+  const isLoggedIn = await isAuth();
 
-  if (!userInfo) {
+  if (isLoggedIn) {
+    var { role } = decodeJwt(token!);
+  }
+  // Prevent redirect loops by checking the current path
+  if (!isLoggedIn && pathname !== "/") {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
+  if (role === "user" && pathname !== "/user/dashboard") {
+    return NextResponse.redirect(new URL("/user/dashboard", req.url));
+  }
+  if (role === "agent" && pathname !== "/agent/dashboard") {
+    return NextResponse.redirect(new URL("/agent/dashboard", req.url));
+  }
+  if (role === "admin" && pathname !== "/admin/") {
+    return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/user/:path*"],
+  matcher: ["/user/:path*", "/agent/:path*"],
 };
