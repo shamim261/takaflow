283014e2fc0cb26db00c713308caeeb332bd46aa
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-20 12:26:36 +0600

    added statistics in dashboard

--- app/admin/(private)/dashboard/page.tsx
+++ app/admin/(private)/dashboard/page.tsx
@@ -1,67 +1,13 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  ArrowDownRight,
-  ArrowUpRight,
-  CreditCard,
-  DollarSign,
-  UserPlus,
-  Users,
-} from "lucide-react";
+import { Card } from "@/components/ui/card";
 import TransactionsTable from "../transactions/TransactionTable";
+import Statistics from "./Statistics";
 
 const Dashboard = () => {
   // Mock data for statistics
-  const statistics = [
-    { title: "Total Users", value: "10,483", change: "+12%", icon: Users },
-    {
-      title: "New Users (30 days)",
-      value: "1,247",
-      change: "+18%",
-      icon: UserPlus,
-    },
-    {
-      title: "Total Transactions",
-      value: "$2.4M",
-      change: "+8%",
-      icon: CreditCard,
-    },
-    { title: "Revenue", value: "$87,345", change: "-3%", icon: DollarSign },
-  ];
 
   return (
     <div className="max-w-md md:max-w-7xl mx-auto my-2">
-      <div className="mb-6" id="statistics">
-        <h2 className="text-2xl font-bold mb-4 text-blue-500">Statistics</h2>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {statistics.map((stat, index) => (
-            <Card key={index}>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  {stat.title}
-                </CardTitle>
-                <stat.icon className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">{stat.value}</div>
-                <p
-                  className={`text-xs ${
-                    stat.change.startsWith("+")
-                      ? "text-green-600"
-                      : "text-red-600"
-                  }`}
-                >
-                  {stat.change.startsWith("+") ? (
-                    <ArrowUpRight className="inline h-4 w-4" />
-                  ) : (
-                    <ArrowDownRight className="inline h-4 w-4" />
-                  )}
-                  {stat.change}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
+      <Statistics />
       <div>
         <h2 className="text-2xl font-bold mb-4 text-blue-500">
           Recent Transactions
---
+++ app/api/admin/statistics/route.ts
@@ -0,0 +1,52 @@
+import Transaction from "@/models/transactionModel";
+import { getUser } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  const userInfo = await getUser();
+  if (!userInfo?.isAdmin) {
+    return NextResponse.json({ error: "Unauthorized!" }, { status: 401 });
+  }
+  try {
+    await connectDB();
+    const statistics = await Transaction.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalTransaction: { $sum: "$amount" },
+          totalRevenue: { $sum: "$fee" },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          pipeline: [{ $count: "totalUsers" }],
+          as: "userCount",
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          pipeline: [{ $match: { role: "agent" } }, { $count: "totalUser" }],
+          as: "agentCount",
+        },
+      },
+      {
+        $unwind: {
+          path: "$userCount",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+    ]);
+
+    return NextResponse.json(statistics);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Internal Server Error!" },
+      { status: 500 }
+    );
+  }
+};
