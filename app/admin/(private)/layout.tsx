0eaf8af6b9e8c11f161fe48a4a9c8b83e004761e
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-20 13:17:33 +0600

    uncommented

--- app/admin/(private)/layout.tsx
+++ app/admin/(private)/layout.tsx
@@ -3,9 +3,9 @@ import { PropsWithChildren } from "react";
 import Navbar from "./Navbar";
 const Layout = async ({ children }: PropsWithChildren) => {
   const info = await getUser();
-  // if (!info) {
-  //   return null;
-  // }
+  if (!info) {
+    return null;
+  }
 
   return (
     <Navbar user={info} className="font-signika">
